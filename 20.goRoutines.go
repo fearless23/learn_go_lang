b6a2c3ab48fb85c8fbd1102da3179d1de0a9bd05
fearless <jaspreetsingh2379@gmail.com>
2020-01-07 05:43:30 +0530

    Chapter 20 Added
    Go routines and best practices

--- 20.goRoutines.go
+++ 20.goRoutines.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"sync"
+)
 
+var wg = sync.WaitGroup{}
+var counter = 0
+var m = sync.RWMutex{}
+
+func main() {
+	msg := "hello"
+
+	// wg.Add(1)
+	// go sayHello(msg)
+	// msg = "no,hello"
+	// time.Sleep(100 * time.Millisecond)
+
+	for i := 0; i < 10; i++ {
+		wg.Add(2)
+		m.RLock()
+		go sayHello(msg)
+		m.Lock()
+		go increment()
+	}
+
+	wg.Wait()
+
+	// runtime.GOMAXPROCS(100)
+	// x := runtime.GOMAXPROCS(-1)
+
+}
+
+func sayHello(msg string) {
+	gologv("Counter", counter)
+	gologv("msg", msg)
+	m.RUnlock()
+	wg.Done()
+}
+
+func increment() {
+	counter++
+	m.Unlock()
+	wg.Done()
 }
